13205d582f5889d7e7053eecc7c781dd5e506fc7
yumzen <yeom.ji.eun35@gmail.com>
2024-05-16 12:03:04 +0900

    setting

---
+++ hongik-hospital/src/main/java/hongikhospital/example/hongikhospital/HongikHospitalApplication.java
@@ -0,0 +1,13 @@
+package hongikhospital.example.hongikhospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongikHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongikHospitalApplication.class, args);
+	}
+
+}
---
+++ hongik-hospital/src/test/java/hongikhospital/example/hongikhospital/HongikHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package hongikhospital.example.hongikhospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongikHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
